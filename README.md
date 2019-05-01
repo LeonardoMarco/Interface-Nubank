7f11d77034d44d210abbf8b6fe4f8d8b8f9c4b08
LeonardoMarco <leo26alves@yahoo.com.br>
2019-05-01 15:30:57 -0300

    Finalized

---
+++ README.md
@@ -0,0 +1,13 @@
+# Nubank Interface
+Recreating the nubank interface with react-native :blush:
+![Screenshot](interface.png)
+
+---
+
+### installation ###
+```sh
+yarn install #ou npm install
+react-native link react-native-gesture-handler
+react-native link react-native-vector-icons
+react-native run-android
+```
---
+++ src/pages/Main/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { StatusBar } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
+import Header from '~/components/Header'
+import Tabs from '~/components/Tabs'
+import Menu from '~/components/Menu'
+
+import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
+
+export default function Main(){
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const AnimatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true }
+  )
+
+  function onHandlerStateChange(event) {
+    if(event.nativeEvent.oldState == State.ACTIVE){
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      if(translationY >= 100 ){
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, { 
+        toValue: opened ? 380 : 0,
+        duarion: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;  
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+      
+    }
+  }
+
+  return(
+    <>
+      <StatusBar barStyle="light-content"  backgroundColor="#8B10AE" />
+      <Container> 
+        <Header />
+
+        <Content>
+          <Menu translateY={translateY} />
+          
+          <PanGestureHandler
+            onGestureEvent={AnimatedEvent}
+            onHandlerStateChange={onHandlerStateChange}
+          >
+            <Card style={{
+              transform: [{
+                translateY: translateY.interpolate({
+                  inputRange: [-200, 0, 380],
+                  outputRange: [-50, 0, 380],
+                  extrapolate: 'clamp',
+                }),
+              }]
+            }}>
+              <CardHeader>
+                <Icon name="attach-money" size={28} color="#666" />
+                <Icon name="visibility-off" size={28} color="#666" />
+              </CardHeader>
+              <CardContent>
+                <Title>Saldo disponível</Title>
+                <Description>R$ 197.000,00</Description>
+              </CardContent>
+              <CardFooter>
+                <Annotation>
+                  Transferencia de R$ 20,00 recebida de Marco Leonardo Silva hoje às 10:00h
+                </Annotation>
+              </CardFooter>
+            </Card>
+          </PanGestureHandler>
+        </Content>
+
+        <Tabs translateY={translateY} />
+      </Container>
+    </>
+  )
+}
